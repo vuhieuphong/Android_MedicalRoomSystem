9348cf2a52d5acf77157cba4be1131be44169835
vuhieuphong <vuhieuphong99@gmail.com>
2020-03-10 19:19:27 -0400

    base GUI created

--- app/src/main/java/com/example/hieuphong_vu_duc_dang_comp304_sec003_lab04/MainActivity.java
+++ app/src/main/java/com/example/hieuphong_vu_duc_dang_comp304_sec003_lab04/MainActivity.java
@@ -11,8 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        AppDatabase db = Room.databaseBuilder(getApplicationContext(),
-                AppDatabase.class, "app-database").build();
     }
 }
